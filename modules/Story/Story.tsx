f0ae6ac7fa63d30ec197c97065e2f129470e44cb
Gijs Nelissen <gijs@prezly.com>
2021-03-26 13:23:44 +0100

    Update homepage + article page

--- modules/Story/Story.tsx
+++ modules/Story/Story.tsx
@@ -6,84 +6,95 @@ type Props = {
     story: ExtendedStory;
 };
 
-const Story: FunctionComponent<Props> = ({ story }) => (
-    <article className="relative py-16 bg-white overflow-hidden">
-        <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
-            <div className="relative h-full text-lg max-w-prose mx-auto" aria-hidden="true">
-                <svg
-                  className="absolute top-12 left-full transform translate-x-32" width="404" height="384"
-                  fill="none" viewBox="0 0 404 384"
-                >
-                    <defs>
-                        <pattern
-                          id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20"
-                          patternUnits="userSpaceOnUse"
-                        >
-                            <rect x="0" y="0" width="4" height="4" className="text-gray-200" fill="currentColor" />
-                        </pattern>
-                    </defs>
-                    <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
-                </svg>
-                <svg
-                  className="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32" width="404"
-                  height="384" fill="none" viewBox="0 0 404 384"
-                >
-                    <defs>
-                        <pattern
-                          id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20"
-                          patternUnits="userSpaceOnUse"
-                        >
-                            <rect x="0" y="0" width="4" height="4" className="text-gray-200" fill="currentColor" />
-                        </pattern>
-                    </defs>
-                    <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
-                </svg>
-                <svg
-                  className="absolute bottom-12 left-full transform translate-x-32" width="404" height="384"
-                  fill="none" viewBox="0 0 404 384"
-                >
-                    <defs>
-                        <pattern
-                          id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20"
-                          patternUnits="userSpaceOnUse"
-                        >
-                            <rect x="0" y="0" width="4" height="4" className="text-gray-200" fill="currentColor" />
-                        </pattern>
-                    </defs>
-                    <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
-                </svg>
-            </div>
-        </div>
-        <div className="relative px-4 sm:px-6 lg:px-8">
-            <div className="text-lg max-w-prose mx-auto">
-                {story.categories.map((category) => (
-                    <span
-                      className="inline-flex items-center px-3 py-0.5 mr-1 rounded-full text-xs font-medium bg-indigo-100 text-indigo-800"
-                    >
-                        {category.display_name}
-                    </span>
-                ))}
+const Story: FunctionComponent<Props> = ({ story }) => {
+    if (!story) {
+        return null;
+    }
 
-                <h1
-                  className="mt-2 block text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl"
-                >
-                    {story.title}
-                </h1>
-                <p className="mt-8 text-xl text-gray-500 leading-8">
-                    {story.subtitle}
-                </p>
+    const {
+        title, subtitle, format_version, content, htmlContent,
+    } = story;
+
+    return (
+
+        <article className="relative py-16 bg-white overflow-hidden">
+            <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+                <div className="relative h-full text-lg max-w-prose mx-auto" aria-hidden="true">
+                    <svg
+                        className="absolute top-12 left-full transform translate-x-32" width="404" height="384"
+                        fill="none" viewBox="0 0 404 384"
+                    >
+                        <defs>
+                            <pattern
+                                id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20"
+                                patternUnits="userSpaceOnUse"
+                            >
+                                <rect x="0" y="0" width="4" height="4" className="text-gray-200" fill="currentColor" />
+                            </pattern>
+                        </defs>
+                        <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
+                    </svg>
+                    <svg
+                        className="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32" width="404"
+                        height="384" fill="none" viewBox="0 0 404 384"
+                    >
+                        <defs>
+                            <pattern
+                                id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20"
+                                patternUnits="userSpaceOnUse"
+                            >
+                                <rect x="0" y="0" width="4" height="4" className="text-gray-200" fill="currentColor" />
+                            </pattern>
+                        </defs>
+                        <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+                    </svg>
+                    <svg
+                        className="absolute bottom-12 left-full transform translate-x-32" width="404" height="384"
+                        fill="none" viewBox="0 0 404 384"
+                    >
+                        <defs>
+                            <pattern
+                                id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20"
+                                patternUnits="userSpaceOnUse"
+                            >
+                                <rect x="0" y="0" width="4" height="4" className="text-gray-200" fill="currentColor" />
+                            </pattern>
+                        </defs>
+                        <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
+                    </svg>
+                </div>
             </div>
-            <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
-                {format_version === 1 && (
-                    // eslint-disable-next-line react/no-danger
-                    <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
-                )}
-                {format_version === 3 && (
-                    <SlateRenderer nodes={JSON.parse(content)} />
-                )}
+            <div className="relative px-4 sm:px-6 lg:px-8">
+                <div className="text-lg max-w-prose mx-auto">
+                    {story.categories.map((category) => (
+                        <span
+                            className="inline-flex items-center px-3 py-0.5 mr-1 rounded-full text-xs font-medium bg-indigo-100 text-indigo-800"
+                        >
+                            {category.display_name}
+                        </span>
+                    ))}
+
+                    <h1
+                        className="mt-2 block text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl"
+                    >
+                        {title}
+                    </h1>
+                    <p className="mt-8 text-xl text-gray-500 leading-8">
+                        {subtitle}
+                    </p>
+                </div>
+                <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+                    {format_version === 1 && (
+                        // eslint-disable-next-line react/no-danger
+                        <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+                    )}
+                    {format_version === 3 && (
+                        <SlateRenderer nodes={JSON.parse(content)} />
+                    )}
+                </div>
             </div>
-        </div>
-    </article>
-);
+        </article>
+    );
+};
 
 export default Story;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import type { FunctionComponent } from 'react';
-import type { Story } from '@prezly/sdk';
 import { GetServerSideProps } from 'next';
 import { getPrezlyApi, withAuthorization } from '@/utils/prezly';
 import Layout from '@/components/Layout';
 import Stories from '@/modules/Stories';
+import type { ExtendedStory } from '@prezly/sdk/dist/types';
 
 type Props = {
     stories: Array<ExtendedStory>;
-}
+};
 
 const IndexPage: FunctionComponent<Props> = ({ stories }) => (
     <Layout>
@@ -17,11 +17,11 @@ const IndexPage: FunctionComponent<Props> = ({ stories }) => (
 
 export const getServerSideProps: GetServerSideProps<Props> = withAuthorization(async (context) => {
     const api = getPrezlyApi(context.req);
-    const stories = await api.getAllStories();
+    const stories = await api.getAllStoriesExtended();
 
     return {
         props: { stories },
     };
-};
+});
 
 export default IndexPage;
