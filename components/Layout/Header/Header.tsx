3656936292517657edf3f42ce21b71da220c0a5f
pablopunk <pablo@pablopunk.com>
2021-03-30 16:40:50 +0200

    Nice looking header

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -1,6 +1,8 @@
-import { FunctionComponent, useState } from 'react';
+/* eslint-disable max-len */
+import { FunctionComponent, useEffect, useState } from 'react';
 import Link from 'next/link';
 import { Category } from '@prezly/sdk/dist/types';
+import { useRouter } from 'next/router';
 
 type Props = {
     categories?: Array<Category>
@@ -8,204 +10,69 @@ type Props = {
 
 const Header: FunctionComponent<Props> = ({ categories }) => {
     const [menuOpen, menuOpenSet] = useState(false);
+    const { asPath } = useRouter();
 
     const toggleMenu = () => menuOpenSet(!menuOpen);
 
+    useEffect(() => {
+        menuOpenSet(false); // close menu on navigation
+    }, [asPath]);
+
     return (
-        <header className="relative bg-white">
-            <div className="absolute inset-0 shadow z-30 pointer-events-none" aria-hidden="true" />
-            <div className="relative z-20">
-                <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
-                    <div>
-                        <Link href="/" passHref>
-                            <a className="flex">
-                                <span className="sr-only">Heartbeat</span>
-                                <img className="h-24 w-auto" src="https://cdn.uc.assets.prezly.com/1a393240-1739-487c-a4bc-ed3c36f6b300/-/quality/best/-/format/auto/" alt="" />
-                            </a>
-                        </Link>
-                    </div>
-                    <div className="-mr-2 -my-2 md:hidden">
-                        <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
-                            <span className="sr-only">Open menu</span>
-                            <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+        <header>
+            <div className="w-full text-gray-700 bg-white">
+                <div className="flex flex-col max-w-screen-xl mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
+                    <div className="p-4 flex flex-row items-center justify-between">
+                        <div>
+                            <Link href="/" passHref>
+                                <a className="flex">
+                                    <span className="sr-only">Heartbeat</span>
+                                    <img className="h-24 w-auto" src="https://cdn.uc.assets.prezly.com/1a393240-1739-487c-a4bc-ed3c36f6b300/-/quality/best/-/format/auto/" alt="" />
+                                </a>
+                            </Link>
+                        </div>
+                        <button className="md:hidden rounded-lg focus:outline-none focus:shadow-outline" onClick={toggleMenu} type="button">
+                            <svg fill="currentColor" viewBox="0 0 20 20" className="w-6 h-6">
+                                {menuOpen ? (
+                                    <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                                ) : (
+                                    <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clipRule="evenodd" />
+                                )}
                             </svg>
                         </button>
                     </div>
-                    <div className=" md:flex-1 md:flex md:items-center md:justify-between">
-                        <nav className="flex space-x-10" />
-                        <div className="flex items-center md:ml-12">
-                            <div className="relative">
-                                <button
-                                    type="button"
-                                    className="text-gray-500 group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                    aria-expanded="false"
-                                    onClick={toggleMenu}
-                                >
-                                    <span>Categories</span>
-
-                                    <svg
-                                        className="text-gray-400 ml-2 h-5 w-5 group-hover:text-gray-500"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 20 20"
-                                        fill="currentColor"
-                                        aria-hidden="true"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                            clipRule="evenodd"
-                                        />
-                                    </svg>
-                                </button>
-
-                                <div
-                                    className={`${menuOpen ? 'visible' : 'hidden'} absolute z-10 right transform -translate-x-1/2 mt-4 w-screen max-w-md sm:px-0`}
-                                >
-                                    <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                                        <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                                            {
-                                                categories.map((category) => {
-                                                    const locales = Object.keys(category.i18n);
-                                                    const locale = locales.filter(
-                                                        (l) => !!category.i18n[l].slug,
-                                                    )[0] || locales[0];
-
-                                                    return (
-                                                        <Link key={category.id} href={`/category/${category.i18n[locale].slug}`}>
-                                                            <a className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" onClick={toggleMenu}>
-                                                                <div className="ml-4">
-                                                                    <p className="text-base font-medium text-gray-900">
-                                                                        {category.display_name}
-                                                                    </p>
-                                                                    <p className="mt-1 text-sm text-gray-500">
-                                                                        {category.display_description}
-                                                                    </p>
-                                                                </div>
-                                                            </a>
-                                                        </Link>
-                                                    );
-                                                })
-                                            }
-                                        </div>
+                    <nav className="relative flex-col flex-grow pb-4 md:pb-0 md:flex md:justify-end md:flex-row">
+                        <button onClick={toggleMenu} className="hidden md:flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg md:w-auto md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" type="button">
+                            <span>Categories</span>
+                            <svg fill="currentColor" viewBox="0 0 20 20" className="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
+                        </button>
+                        {
+                            menuOpen && (
+                                <div className="absolute z-10 top-0 md:top-8 right-0 w-full mt-2 md:py-4 origin-top-right rounded-md shadow-lg md:w-80">
+                                    <div className="px-2 py-2 bg-white rounded-md shadow">
+                                        {categories.map((category) => {
+                                            const locales = Object.keys(category.i18n);
+                                            const locale = locales.filter((l) => !!category.i18n[l].slug)[0] || locales[0];
+                                            return (
+                                                <Link href={`/category/${category.i18n[locale].slug}`}>
+                                                    <a
+                                                        className="block px-4 py-2 bg-transparent rounded-lg md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                                                    >
+                                                        <span className="font-bold">
+                                                            {category.display_name}
+                                                        </span>
+                                                        <p className="mt-1 text-sm text-gray-500">
+                                                            {category.display_description}
+                                                        </p>
+                                                    </a>
+                                                </Link>
+                                            );
+                                        })}
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-                <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-                    <div className="pt-5 pb-6 px-5 sm:pb-8">
-                        <div className="flex items-center justify-between">
-                            <div>
-                                <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
-                            </div>
-                            <div className="-mr-2">
-                                <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                                    <span className="sr-only">Close menu</span>
-                                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </div>
-                        <div className="mt-6 sm:mt-8">
-                            <nav>
-                                <div className="grid gap-7 sm:grid-cols-2 sm:gap-y-8 sm:gap-x-4">
-                                    <a href="#" className="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
-                                        <div className="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-500 text-white sm:h-12 sm:w-12">
-                                            <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                                            </svg>
-                                        </div>
-                                        <div className="ml-4 text-base font-medium text-gray-900">
-                                            Analytics
-                                        </div>
-                                    </a>
-
-                                    <a href="#" className="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
-                                        <div className="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-500 text-white sm:h-12 sm:w-12">
-                                            <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
-                                            </svg>
-                                        </div>
-                                        <div className="ml-4 text-base font-medium text-gray-900">
-                                            Engagement
-                                        </div>
-                                    </a>
-
-                                    <a href="#" className="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
-                                        <div className="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-500 text-white sm:h-12 sm:w-12">
-                                            <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-                                            </svg>
-                                        </div>
-                                        <div className="ml-4 text-base font-medium text-gray-900">
-                                            Security
-                                        </div>
-                                    </a>
-
-                                    <a href="#" className="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
-                                        <div className="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-500 text-white sm:h-12 sm:w-12">
-                                            <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
-                                            </svg>
-                                        </div>
-                                        <div className="ml-4 text-base font-medium text-gray-900">
-                                            Integrations
-                                        </div>
-                                    </a>
-                                </div>
-                                <div className="mt-8 text-base">
-                                    <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                        {' '}
-                                        View all products
-                                        <span aria-hidden="true">&rarr;</span>
-                                    </a>
-                                </div>
-                            </nav>
-                        </div>
-                    </div>
-                    <div className="py-6 px-5">
-                        <div className="grid grid-cols-2 gap-4">
-                            <a href="#" className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700">
-                                Pricing
-                            </a>
-
-                            <a href="#" className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700">
-                                Docs
-                            </a>
-
-                            <a href="#" className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700">
-                                Company
-                            </a>
-
-                            <a href="#" className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700">
-                                Resources
-                            </a>
-
-                            <a href="#" className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700">
-                                Blog
-                            </a>
-
-                            <a href="#" className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700">
-                                Contact Sales
-                            </a>
-                        </div>
-                        <div className="mt-6">
-                            <a href="#" className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-                                Sign up
-                            </a>
-                            <p className="mt-6 text-center text-base font-medium text-gray-500">
-                                Existing customer?
-                                <a href="#" className="text-indigo-600 hover:text-indigo-500">
-                                    Sign in
-                                </a>
-                            </p>
-                        </div>
-                    </div>
+                            )
+                        }
+                    </nav>
                 </div>
             </div>
         </header>
