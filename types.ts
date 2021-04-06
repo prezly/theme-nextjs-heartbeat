fa0b3cbfc14047ba7206464f8831cdb382ac3783
Gijs Nelissen <gijs@prezly.com>
2021-04-06 12:25:18 +0200

    Add discourse credentials to types and .env.example

--- types.ts
+++ types.ts
@@ -2,4 +2,7 @@ export interface Env {
     NODE_ENV: 'production' | 'development' | 'test';
     PREZLY_ACCESS_TOKEN: string;
     PREZLY_NEWSROOM_UUID: string;
+    DISCOURSE_USERNAME: string;
+    DISCOURSE_API_URL: string;
+    DISCOURSE_API_KEY: string;
 }
