85e9028a913443c6a81e2417049f83d67c0633b9
Camil Blanaru <camil@edka.io>
2021-06-15 13:20:15 +0200

    Set Cache-Control header

--- next.config.js
+++ next.config.js
@@ -6,5 +6,19 @@ module.exports = {
     }
 
     return config
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          }
+        ],
+      },
+    ]
+  },
 }
