c74f04dbdd7004d67f3c7c7b59f3a6b78ae1a8b4
Oleg Semyonov <oleg@prezly.com>
2022-03-21 13:55:44 +0300

    Fix mismatched types

--- modules/InfiniteStories/lib/useInfiniteStoriesLoading.ts
+++ modules/InfiniteStories/lib/useInfiniteStoriesLoading.ts
@@ -1,17 +1,17 @@
-import type { Category, Story } from '@prezly/sdk';
+import type { Category } from '@prezly/sdk';
 import type { LocaleObject } from '@prezly/theme-kit-nextjs';
 import { useCurrentLocale } from '@prezly/theme-kit-nextjs';
 import { useEffect } from 'react';
 
 import { useInfiniteLoading } from '@/hooks';
-import type { PaginationProps } from 'types';
+import type { PaginationProps, StoryWithImage } from 'types';
 
 async function fetchStories(
     page: number,
     pageSize: number,
     category?: Category,
     locale?: LocaleObject,
-): Promise<{ stories: Story[] }> {
+): Promise<{ stories: StoryWithImage[] }> {
     const result = await fetch('/api/fetch-stories', {
         method: 'POST',
         headers: {
@@ -38,25 +38,26 @@ async function fetchStories(
 }
 
 export function useInfiniteStoriesLoading(
-    initialStories: Story[],
+    initialStories: StoryWithImage[],
     pagination: PaginationProps,
     category?: Category,
 ) {
     const currentLocale = useCurrentLocale();
 
-    const { canLoadMore, data, isLoading, loadMore, resetData } = useInfiniteLoading<Story>({
-        fetchingFn: async (nextPage: number) => {
-            const { stories } = await fetchStories(
-                nextPage,
-                pagination.pageSize,
-                category,
-                currentLocale,
-            );
-            return stories;
-        },
-        initialData: initialStories,
-        pagination,
-    });
+    const { canLoadMore, data, isLoading, loadMore, resetData } =
+        useInfiniteLoading<StoryWithImage>({
+            fetchingFn: async (nextPage: number) => {
+                const { stories } = await fetchStories(
+                    nextPage,
+                    pagination.pageSize,
+                    category,
+                    currentLocale,
+                );
+                return stories;
+            },
+            initialData: initialStories,
+            pagination,
+        });
 
     useEffect(() => {
         if (category?.id) {
