7a47166c4fa880849e45d080f41042d6f4869e95
Oleg Semyonov <oleg@prezly.com>
2022-03-22 17:34:00 +0300

    Update theme-kit to allow operation without `i18n` config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-const { DUMMY_DEFAULT_LOCALE } = require('@prezly/theme-kit-nextjs');
 const { withSentryConfig } = require('@sentry/nextjs');
 
 /**
@@ -49,17 +48,6 @@ const moduleExports = {
                 ],
             },
         ];
-    },
-    i18n: {
-        // These are all the locales you want to support in
-        // your application
-        locales: [DUMMY_DEFAULT_LOCALE],
-        // This is the default locale you want to be used when visiting
-        // a non-locale prefixed path e.g. `/hello`
-        // We use Pseudo locale used for localization testing, to reliably determine if we need to fallback to the default newsroom language
-        defaultLocale: DUMMY_DEFAULT_LOCALE,
-        // Default locale detection is disabled, since the locales would be determined by Prezly API
-        localeDetection: false,
     },
 };
 
